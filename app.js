821de2294ff011df84d881ebd912bc1c0fa8d850
oepr-93 <99208367+oepr-93@users.noreply.github.com>
2022-05-02 12:56:01 -0500

    Add files via upload
    Proyecto creado, dependencias ENV y Trello añadidas

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+/*if(!process.env.TOKEN && !process.env.KEY){
+  throw new Error('No hay configuración con Api Key y con Token')
+}
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "6264e42be72d295e64f5c083",
+  function (error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });*/
+
+const Trello = require("./main");
+var trello = new Trello("key", "token");
+var cardTitle = `Card Nueva ${new Date()}`;
+
+console.log("Ejecutando!");
+
+trello.addCard(cardTitle, "LaunchX Card Description", "IdList",
+  function (error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });
